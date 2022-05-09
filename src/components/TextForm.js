8dcb24511ab1c6e6eceb9cda8e21d289d99d087b
Abdul-SJ <93318046+Abdul-SJ@users.noreply.github.com>
2022-05-09 12:46:52 +0500

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -56,6 +56,7 @@ export default function TextForm(props) {
         var text = document.getElementById('myBox');
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();      // to remove selection on copy
         props.showAlert("Copied to Clipboard", "success");
     }
     //to handle extraspaces
@@ -73,25 +74,22 @@ export default function TextForm(props) {
             <div className="my-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#343a40':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Upper Case</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleDownClick}>Convert to Lower Case</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={capitalFirstLetter}>Capital First Letter</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleDarkTheme}>Dark Mode</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleLightTheme}>Light Mode</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Upper Case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleDownClick}>Convert to Lower Case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={capitalFirstLetter}>Capital First Letter</button>
+            <button className="btn btn-primary mx-2 my-1" onClick={handleDarkTheme}>Dark Mode</button>
+            <button className="btn btn-primary mx-2 my-1" onClick={handleLightTheme}>Light Mode</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
         </div>
         <div className="container mt-4">
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words & {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes Read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words & {text.length} characters</p>    {/**.filter((element)=>{return element.length!==0}) to fix 1 word count error */}
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes Read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter something in the Textbox above to preview it here :)"}</p>  {/*using turnary operators here to display placeholder: if the length of text is greater than 0 than display text else show this string"Enter...." */}
+            <p>{text.length>0?text:"Nothing to preview!"}</p>  {/*using turnary operators here to display placeholder: if the length of text is greater than 0 than display text else show this string"Enter...." */}
         </div>
         </>
     )
 }
-// one curly brace {} means we are writing javascript code in html & {{}} two curly brace means we are writing object in javascript
-
-//change language mode from the bottom right {} Javascript or {} Javascript React
